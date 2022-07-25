59dfbeb1cbf4a2eb6525498f4dbcc5db8f2410b8
6epotato <aarifurrahman93@gmail.com>
2022-07-25 23:16:12 +0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './pages/Home/Header';
 
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 import Footer from './Shared/Footer';
 
 function App() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
+      {/* <Home></Home> */}
+
       <Footer />
     </div>
   );
