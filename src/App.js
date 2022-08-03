c29679cea62bb9b8b63984d7c2d7ac6eb4030d5c
Azizur Rahman <azizurrahman.zero@gmail.com>
2022-08-04 00:20:41 +0600

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AboutUs from "./pages/About/AboutUs";
 import Cards from "./pages/Cards/Cards";
-
+import Blog from "./pages/Blog/Blog";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/Login/SignUp";
@@ -18,12 +18,12 @@ function App() {
         <Route path="/home" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signUp" element={<SignUp />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
         <Route path="/cards" element={<Cards />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
       </Routes>
 
       <Footer />
-
     </div>
   );
 }
---
+++ src/pages/Blog/Blog.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Articles from './Articles';
+import './Blog.css'
+import BlogFeature from './BlogFeature';
+
+const Blog = () => {
+    return (
+        <div className='blog-container  h-auto'>
+           <header className='z-50 w-full lg:h-[50vh] h-[60vh] flex justify-center items-center text-accent'>
+           <div>
+            <h1 className=' text-5xl text-center '>Thoughts, stories and ideas</h1>
+            <p className='text-center lg:w-5/12 mx-auto mt-5 leading-[1.8] text-lg'>Arate is Ghost theme with a sleek and modern design for a personal fashion or lifestyle blog, an online journal or a magazine.</p>
+           </div>
+           </header>
+           <main className='mt-20'>
+            <BlogFeature />
+            <Articles />
+           </main>
+        </div>
+    );
+};
+
+export default Blog;
