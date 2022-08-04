4efbd1d607e8c801fa55d0643704b4cb3331183f
6epotato <aarifurrahman93@gmail.com>
2022-08-04 17:39:51 +0600

    camera app done without check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import {useLocation} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import "./App.css";
 import Blog from "./pages/Blog/Blog";
 import Cards from "./pages/Cards/Cards";
@@ -14,35 +14,36 @@ import WelcomePage from "./pages/Dashboard/WelcomePage";
 import Profile from "./pages/Dashboard/Profile";
 import Review from "./pages/Dashboard/Review";
 import Service from "./pages/Service/Service";
+import Camera from "./pages/Login/Camera";
 
 
 
 function App() {
- const {pathname}=   useLocation()
+  const { pathname } = useLocation()
   return (
-    <div className={`mx-auto max-w-7xl ${pathname.includes("dashboard")&&"py-2"}`}>
-     {!pathname.includes("dashboard")&& <Menubar></Menubar>}
+    <div className={`mx-auto max-w-7xl ${pathname.includes("dashboard") && "py-2"}`}>
+      {!pathname.includes("dashboard") && <Menubar></Menubar>}
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/services" element={<Service />}></Route>
-        <Route path="/login" element={<Login />}></Route> 
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/camera" element={<Camera />}></Route>
         <Route path="/signUp" element={<SignUp />}></Route>
         <Route path="/blog" element={<Blog />}></Route>
         <Route path="/cards" element={<Cards />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
-        <Route path="/dashboard" element={<Dashboard/>}>
-        <Route index element={<WelcomePage />} />
-        <Route path="dashboard" element={<WelcomePage />} />
-        <Route path="profile" element={<Profile />} />
-        
-        <Route path="review" element={<Review />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<WelcomePage />} />
+          <Route path="dashboard" element={<WelcomePage />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="review" element={<Review />} />
         </Route>
-       
+
 
       </Routes>
-      {!pathname.includes("dashboard")&& <Footer></Footer>}
-      
+      {!pathname.includes("dashboard") && <Footer></Footer>}
+
 
     </div>
   );
