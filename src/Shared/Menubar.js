f39e54bb628c2d13d2c3b88cc3036100b6a6f578
Azizur Rahman <azizurrahman.zero@gmail.com>
2022-08-09 01:47:47 +0600

    Menu Done

--- src/Shared/Menubar.js
+++ src/Shared/Menubar.js
@@ -1,58 +1,89 @@
 import { signOut } from "firebase/auth";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import Logo from '../assets/images/logo/logo.png'
-
+import Logo from "../assets/images/logo/logo.svg";
 import { Link } from "react-router-dom";
 import auth from "../firebase.init";
+import "../App.css";
 
 const Menubar = () => {
   const [user] = useAuthState(auth);
-  
 
   const logout = () => {
     signOut(auth);
-    
   };
 
-
-  const menuItem = <>
-    <li><Link to="/home"><img className="h-6" src={Logo} alt="" /></Link></li>
-    <li><Link to="/">Home</Link></li>
-    
-    <li><Link to="/blog">Blog</Link></li>
-    
-    <li className=' z-50' tabindex="0">
-        <a>
+  const menuItem = (
+    <>
+      <li>
+        <Link to="/home" className="p-0 mr-4 logo">
+          <img className="max-w-none w-9" src={Logo} alt="logo" />
+        </Link>
+      </li>
+      <li>
+        <Link to="/" className="font-semibold font-barlow py-0">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/blog" className="font-semibold font-barlow py-0">
+          Blog
+        </Link>
+      </li>
+      <li className="z-50 font-semibold font-barlow py-0" tabIndex="0">
+        <Link to="#" className="py-0">
           Company
-          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </a>
-        <ul className="p-2 ">
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/team">Team</Link></li>
+          <svg
+            className="fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+          >
+            <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+          </svg>
+        </Link>
+        <ul className="px-4 pt-6 pb-4 bg-white">
+          <li>
+            <Link to="/about" className="font-semibold font-barlow py-2">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link to="/team" className="font-semibold font-barlow py-2">
+              Team
+            </Link>
+          </li>
         </ul>
       </li>
-
-    <li><Link to="/cards">Cards</Link></li>
-    <li><Link to="/services">Services</Link></li>
-    
-    {user && (
+      <li>
+        <Link to="/cards" className="font-semibold font-barlow py-0">
+          Cards
+        </Link>
+      </li>
+      <li>
+        <Link to="/services" className="font-semibold font-barlow py-0">
+          Services
+        </Link>
+      </li>
+      {user && (
         <li>
-          <li><Link to="/dashboard">Dashboard</Link></li>
+          <li>
+            <Link to="/dashboard" className="font-semibold font-barlow py-0">
+              Dashboard
+            </Link>
+          </li>
         </li>
       )}
-    
-    
-  
-  </>
-
+    </>
+  );
 
   return (
     <div>
-      <div className="navbar bg-[#073A42]  text-[#F2F2F2]">
+      <div className="navbar bg-[#ffff] text-[#323232] rounded-2xl py-4 px-5">
         <div className="navbar-start ">
           <div className="dropdown">
-            <label tabindex="0" className="btn btn-ghost lg:hidden">
+            <label tabIndex="0" className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -69,36 +100,33 @@ const Menubar = () => {
               </svg>
             </label>
             <ul
-              tabindex="0"
+              tabIndex="0"
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-primary text-accent rounded-box w-52"
             >
               {menuItem}
-              
             </ul>
           </div>
-          
-          <ul className="menu hidden lg:flex menu-horizontal p-0">{menuItem}</ul>
+          <ul className="menu hidden lg:flex menu-horizontal p-0">
+            {menuItem}
+          </ul>
         </div>
-
         <div className="navbar-end">
-       
-          <ul
-              tabindex="0"
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow  text-accent rounded-box w-52"
-            >
-             
-<li className="justify-center items-center">
-        {user ? (
-          <button onClick={logout} className="btn btn-ghost capitalize">
-            Sign Out
-          </button>
-        ) : (
-          <Link className="btn btn-primary px-14 border-none bg-[#F8BF88] text-[#073A42] font-bold" to="/login">Login</Link>
-        )}
-      </li>
-            </ul>
-
-
+          <ul>
+            <li>
+              {user ? (
+                <button onClick={logout} className="btn btn-ghost capitalize">
+                  Sign Out
+                </button>
+              ) : (
+                <Link
+                  className="btn btn-primary px-14 border-none rounded-md bg-[#F8BF88] hover:bg-[#f8be88c1] text-[#073A42] font-bold font-roboto"
+                  to="/login"
+                >
+                  Log In
+                </Link>
+              )}
+            </li>
+          </ul>
         </div>
       </div>
     </div>
--- src/pages/Landingpage/Header.js
+++ src/pages/Landingpage/Header.js
@@ -1,81 +1,119 @@
-import React from 'react';
-import Menubar from '../../Shared/Menubar';
-import BankAdvantage from './BankAdvantage';
-import {FiChevronRight} from 'react-icons/fi'
+import React from "react";
+import Menubar from "../../Shared/Menubar";
+import BankAdvantage from "./BankAdvantage";
+import { FiChevronRight } from "react-icons/fi";
 
 const Header = () => {
-    return (
-        <header className='bg-[rgb(7,58,66)] min-h-screen lg:px-28  px-4'>
-            <div className='walpaper'>
-
-            <div><span className='dot'></span> </div>
-            <div><span className='dot'></span> </div>
-          
+  return (
+    <header className="bg-[rgb(7,58,66)] min-h-screen lg:px-20 px-4 pt-10">
+      <div className="walpaper">
+        <div>
+          <span className="dot"></span>{" "}
+        </div>
+        <div>
+          <span className="dot"></span>{" "}
+        </div>
+      </div>
+      <Menubar />
+      <div className="grid md:grid-cols-2 relative z-50 mt-4 lg:mt-40">
+        <div className=" ">
+          <h1 className="text-white text-4xl lg:text-8xl font-bold">
+            Your Next <br /> Online Bank.
+          </h1>
+          <hr className="h-[3px] w-[80%] my-10 bg-white" />
+          <div className="grid grid-cols-3 lg:w-[95%]">
+            <BankAdvantage advantage="Faster finance transaction" score="6X" />
+            <BankAdvantage
+              advantage="Faster finance transaction"
+              score="-15%"
+            />
+            <BankAdvantage advantage="Faster finance transaction" score="3M+" />
+          </div>
+          <div className="mt-16 flex relative z-50 items-center gap-x-3 lg:gap-x-10">
+            <button className="btn btn-primary  py-3 rounded-none h-auto text-[#073A42] bg-[#F8BF88] border-none font-bold">
+              Download Now
+            </button>
+            <div className="flex items-center gap-x-3 ">
+              <img
+                className="h-14"
+                src="https://cdn.picpng.com/icon/play-icon-free-wallpaper-vector-65968.png"
+                alt=""
+              />
+              <span className="text-white font-semibold text-lg">
+                How its work
+              </span>
             </div>
-            <Menubar />
-            <div className='grid md:grid-cols-2 relative z-50    mt-4 lg:mt-10'>
-                 <div className=' '>
-                    <h1 className='text-white text-4xl lg:text-8xl font-bold'>Your Next <br /> Online Bank.</h1>
-                    <hr className='h-[3px] w-[80%] my-10 bg-white' />
-                    <div className='grid grid-cols-3 lg:w-[95%]'>
-                        <BankAdvantage advantage="Faster finance transaction" score="6X" />
-                        <BankAdvantage advantage="Faster finance transaction" score="-15%" />
-                        <BankAdvantage advantage="Faster finance transaction" score="3M+" />
-                    </div>
-                    <div className='mt-16 flex relative z-50 items-center gap-x-3 lg:gap-x-10'>
-                    <button className='btn btn-primary  py-3 rounded-none h-auto text-[#073A42] bg-[#F8BF88] border-none font-bold'>Download Now</button>
-                    <div className='flex items-center gap-x-3 '>
-                       <img className='h-14' src="https://cdn.picpng.com/icon/play-icon-free-wallpaper-vector-65968.png" alt="" />
-                       <span className='text-white font-semibold text-lg'>How its work</span>
-                    </div>
-                    </div>
-                 </div>
-                 <div className='header-left my-20 lg:my-0'>
-                     <div className='bg-white banking   w-[330px] py-6 mx-auto'>
-                        <div className='px-6'>
-                        <p className='text-md'>Log in to Internet Banking</p>
-                        <div className='flex gap-x-3 my-3'>
-                            <button className='btn btn-primary px-8 py-3 h-auto text-[#073A42] bg-[#F8BF88] border-none font-bold'>Personal</button>
-                            <button className='btn btn-primary px-8 py-5 h-auto bg-[#FF9F88] border-none text-[#073A42] font-bold'>Business</button>
-                        </div>
-                        </div>
-                   
-                        <p className='my-5 flex items-center justify-between px-6'><span className='w-9/12'>Your Car Finance  options </span> <FiChevronRight /></p>
-                        <hr />
-                        <p className='my-5 flex items-center justify-between px-6'><span className='w-9/12'>Check your credit card eligibilty </span> <FiChevronRight /></p>
-                        <hr />
-                        <p className='my-5 flex items-center justify-between px-6'><span className='w-9/12'>Coronavirus: Help and Support </span> <FiChevronRight /></p>
-                        <hr />
-                        <p className='my-5 flex items-center justify-between px-6'><span className='w-9/12'>Find the right mortgage for you </span> <FiChevronRight /></p>
-
-                       
-                     </div>
-                        <div className="wraper">
-                        <div><span className='dot'></span> </div>
-                        <div><span className='dot'></span> </div>
-                       </div>
-                 </div>
+          </div>
+        </div>
+        <div className="header-left my-20 lg:my-0">
+          <div className="bg-white banking   w-[330px] py-6 mx-auto">
+            <div className="px-6">
+              <p className="text-md">Log in to Internet Banking</p>
+              <div className="flex gap-x-3 my-3">
+                <button className="btn btn-primary px-8 py-3 h-auto text-[#073A42] bg-[#F8BF88] border-none font-bold">
+                  Personal
+                </button>
+                <button className="btn btn-primary px-8 py-5 h-auto bg-[#FF9F88] border-none text-[#073A42] font-bold">
+                  Business
+                </button>
+              </div>
             </div>
 
-
-            <div className='grid md:grid-cols-3 lg:mt-96 pb-20 lg:pb-0 mt-52'>
-                <div className='banner-bottom'>
-                  <img className=' mx-auto' src="https://www.thecentralamericangroup.com/wp-content/uploads/2018/10/Man-Transparent.png" alt="" />
-                </div>
-                <div className='flex justify-center items-center'>
-                    <hr className='bg-white  w-[80%] mx-auto h-[1px]' />
-                </div>
-                <div className='flex items-center mt-10 lg:mt-0'>
-                    <div>
-                    <p className='text-lg text-gray-300'>Improve your finances</p>
-                        
-                    <h1 className='text-3xl text-white font-semibold mt-2'>100% flexible online banking platform that gives you more total finance control</h1>
-                    </div>
-                </div>
+            <p className="my-5 flex items-center justify-between px-6">
+              <span className="w-9/12">Your Car Finance options </span>{" "}
+              <FiChevronRight />
+            </p>
+            <hr />
+            <p className="my-5 flex items-center justify-between px-6">
+              <span className="w-9/12">Check your credit card eligibilty </span>{" "}
+              <FiChevronRight />
+            </p>
+            <hr />
+            <p className="my-5 flex items-center justify-between px-6">
+              <span className="w-9/12">Coronavirus: Help and Support </span>{" "}
+              <FiChevronRight />
+            </p>
+            <hr />
+            <p className="my-5 flex items-center justify-between px-6">
+              <span className="w-9/12">Find the right mortgage for you </span>{" "}
+              <FiChevronRight />
+            </p>
+          </div>
+          <div className="wraper">
+            <div>
+              <span className="dot"></span>{" "}
             </div>
-            
-        </header>
-    );
+            <div>
+              <span className="dot"></span>{" "}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid md:grid-cols-3 lg:mt-96 pb-20 lg:pb-0 mt-52">
+        <div className="banner-bottom">
+          <img
+            className=" mx-auto"
+            src="https://www.thecentralamericangroup.com/wp-content/uploads/2018/10/Man-Transparent.png"
+            alt=""
+          />
+        </div>
+        <div className="flex justify-center items-center">
+          <hr className="bg-white  w-[80%] mx-auto h-[1px]" />
+        </div>
+        <div className="flex items-center mt-10 lg:mt-0">
+          <div>
+            <p className="text-lg text-gray-300">Improve your finances</p>
+
+            <h1 className="text-3xl text-white font-semibold mt-2">
+              100% flexible online banking platform that gives you more total
+              finance control
+            </h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
