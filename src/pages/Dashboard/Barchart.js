91293c56449855a69e82a84f5ccdc3405b2f3b7d
Istiak <istieak.hasan1996@gmail.com>
2022-08-10 18:06:49 +0600

    updated chart

--- src/pages/Dashboard/Barchart.js
+++ src/pages/Dashboard/Barchart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
+import { BarChart, CartesianGrid, XAxis, YAxis,ResponsiveContainer, Tooltip, Legend, Bar } from 'recharts';
 
 const Barchart = () => {
     const data = [
@@ -41,18 +41,29 @@ const Barchart = () => {
     ]
 
     return (
-        <div>
-            <h1 className='text-xl font-bold py-6'>Monthely Update</h1>
-            <BarChart width={500} height={250} data={data}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="pv" fill="#8884d8" />
-                <Bar dataKey="uv" fill="#82ca9d" />
-            </BarChart>
+        <div className="h-[45vh]  shadow-2xl rounded-xl px-4  mt-2">
+          <h1 className="text-3xl font-bold text-black mt-2">Overview</h1>
 
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 60,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+
+              <Tooltip />
+
+              <Bar dataKey="pv" fill="#8884d8" />
+              <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
         </div>
     );
 };
