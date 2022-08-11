ffe03b32006b20386480a002967cb58d990d0d12
Istiak <istieak.hasan1996@gmail.com>
2022-08-11 13:50:40 +0600

    fixed user approved problems

--- src/pages/Dashboard/Maneger/CheckInformation.js
+++ src/pages/Dashboard/Maneger/CheckInformation.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const CheckInformation = ({ information, users, setUsers }) => {
   const {
@@ -25,7 +26,23 @@ const CheckInformation = ({ information, users, setUsers }) => {
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data);
+         if(data.insertedId){
+          const url = `http://localhost:4000/users/${id}`;
+
+          fetch(url, {
+            method: 'DELETE'
+          })
+            .then(res => res.json())
+            .then(data => {
+              if (data.deletedCount > 0) {
+               toast.success("User request approved successfully")
+                const remaining = users.filter(user => user._id !== id)
+                setUsers(remaining);
+      
+              }
+              console.log(data)
+            })
+         }
       })
 
 
@@ -33,20 +50,7 @@ const CheckInformation = ({ information, users, setUsers }) => {
 
     
 
-    const url = `http://localhost:4000/users/${id}`;
-
-    fetch(url, {
-      method: 'DELETE'
-    })
-      .then(res => res.json())
-      .then(data => {
-        if (data.deletedCount > 0) {
-
-          const remaining = users.filter(user => user._id !== id)
-          setUsers(remaining);
-
-        }
-      })
+   
 
 
   }
