4235e0050b0c243b3e95e12a9301aa2f05be381c
sbsohag3 <sbsohag3@gmail.com>
2022-08-11 18:27:56 +0600

    delete user

--- src/pages/Dashboard/UserInformation/Deposit.js
+++ src/pages/Dashboard/UserInformation/Deposit.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const Deposit = ({userInfo}) => {
-  const {amount}= userInfo;
+const Deposit = ({ userInfo }) => {
+  const { amount, accountNumber } = userInfo;
   return (
     <>
       <input type="checkbox" id="deposit" class="modal-toggle" />
@@ -13,21 +13,43 @@ const Deposit = ({userInfo}) => {
           >
             ✕
           </label>
-          <div className='px-12'>
-          <h3 class="font-bold text-2xl">
-            Deposit!
-          </h3>
-          
-          
-          <div className="w-full max-w-full my-4 form-control">
-          <input id="account-number" name="accountNumber" className="px-5" value={amount} type="number" disabled />
-                </div>
-          
-          
-          <div className="w-full max-w-full my-4 form-control">
-          <input id="account-number" name="accountNumber" className="px-5" value={amount} type="number" disabled />
-                </div>
-          
+          <div className="px-12">
+            <h3 class="font-bold text-2xl">Deposit!</h3>
+
+            <div className="w-full max-w-full my-4 form-control">
+              <label htmlFor="accountNumber">ACCOUNT NUMBER</label>
+              <input
+                id="account-number"
+                name="accountNumber"
+                className="px-5"
+                value={accountNumber}
+                type="number"
+                disabled
+              />
+            </div>
+
+            <div className="w-full max-w-full my-4 form-control">
+              <label htmlFor="accountNumber">CURRENT BALANCE</label>
+              <input
+                id="account-number"
+                name="accountNumber"
+                className="px-5"
+                value={amount}
+                type="number"
+                disabled
+              />
+            </div>
+
+            <div className="w-full max-w-full my-4 form-control">
+              <label htmlFor="accountNumber">AMOUNT TO DEPOSIT</label>
+              <input
+                id="amount"
+                name="amount"
+                className="px-5"
+                value="amount"
+                type="number"
+              />
+            </div>
           </div>
           <div class="text-center">
             <label for="deposit" class="btn btn-accent ">
